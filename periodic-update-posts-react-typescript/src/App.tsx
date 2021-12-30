f88ff5ee9a7be46c3ccb089f7d546a975ba071ad
Webdev1011 <92213116+Webdev1011@users.noreply.github.com>
2021-12-31 00:12:01 +0530

    Final App Push

--- periodic-update-posts-react-typescript/src/App.tsx
+++ periodic-update-posts-react-typescript/src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import TableList from "./TableList";
 
-function App() {
+interface objPost {
+  id: number;
+  title: string;
+  url: string;
+  created_at: Date;
+  author: string;
+}
+
+export interface TablePorps {
+  posts: objPost[];
+}
+
+const App = () => {
+  const [lastPageNumber, setLastPageNumber] = useState<number>(0);
+  const [posts, setPosts] = useState<objPost[]>([]);
+  const getPosts = async () => {
+    await fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${lastPageNumber}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.hasOwnProperty("hits") && json.hits.length > 0) {
+          json.hits.map((post: any) => {
+            const tmp = {
+              id: post.objectID,
+              title: post.title,
+              url: post.url,
+              created_at: new Date(post.created_at),
+              author: post.author,
+            };
+            setPosts((prevState) => [...prevState, tmp]);
+          });
+        }
+      })
+      .catch((error) => {
+        alert("Posts Finished");
+      });
+  };
+  useEffect(() => {
+    getPosts();
+    const periodic_calls = setInterval(() => {
+      setLastPageNumber((prevState) => prevState + 1);
+    }, 10000);
+    return () => clearInterval(periodic_calls);
+  }, [lastPageNumber]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TableList posts={posts} />} />
+        </Routes>
+      </Router>
+      <div>
+        <h1>Posts</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ periodic-update-posts-react-typescript/src/TableList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TableProps from "./App";
+
+const TableList: React.FC<TableProps> = (posts) => {
+  return <div></div>;
+};
+
+export default TableList;
